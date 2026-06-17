51859f959253387bbfc2d5e06285a149f0bfb5c8
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-06-17 19:14:44 +0500

    Header - 1

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 const Layout = () => {
   return (
     <div>
-      <header>Header</header>
-      <main>
+      <Header />
+      <main className="pt-20">
         <Outlet />
       </main>
       <footer>Footer</footer>
