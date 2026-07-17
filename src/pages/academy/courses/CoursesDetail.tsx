000b633d0602e39744830ea61b15fd5714a4ef60
HasanboyZafarov <dewozto1337@gmail.com>
2026-07-17 11:40:42 +0500

    Implemented useCourse hook in order to get a single course bySlug

--- src/pages/academy/courses/CoursesDetail.tsx
+++ src/pages/academy/courses/CoursesDetail.tsx
@@ -1,7 +1,12 @@
+import useCourse from "@/lib/service/useCourse";
 import { useParams } from "react-router-dom";
 
 const CoursesDetail = () => {
   const { slug } = useParams();
+  const { course, error, isLoading } = useCourse(slug || "");
+
+  console.log(course);
+
   return <div>CoursesDetail: {slug}</div>;
 };
 
